c1aa6e6504f02f13f8e5012a2a6d57c09df96a93
contetes <estelle.contet@reseau.eseo.fr>
2019-01-13 22:39:51 +0100

    Accès à la servlet via web.xml

--- TP_Docker/src/main/java/com/tpdocker/servlets/Accueil.java
+++ TP_Docker/src/main/java/com/tpdocker/servlets/Accueil.java
@@ -3,7 +3,6 @@ package com.tpdocker.servlets;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Accueil
  */
-@WebServlet("/accueil")
 public class Accueil extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final String VUE = "/WEB-INF/jsp/Accueil.jsp";
